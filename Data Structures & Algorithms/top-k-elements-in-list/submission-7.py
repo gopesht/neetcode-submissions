06a1c120c0b2dfbec1b4743f9307df051aa0029a
Gopesh Tulsyan <gopesht@users.noreply.github.com>
2026-08-16 19:42:42 +0100

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count_map = {}
+        for n in nums:
+            count_map[n] = count_map.get(n, 0) + 1
+        
+        buckets = [[] for i in range(len(nums) + 1)]
+
+
+        for key, v in count_map.items():
+            buckets[v].append(key)
+        result = []
+        while k>0:
+            for n in reversed(buckets):
+                if n is not None and k>0:
+                    for i in n:
+                        result.append(i)
+                        k = k - 1
+                        if k == 0:
+                            break
+
+        return result
+        
